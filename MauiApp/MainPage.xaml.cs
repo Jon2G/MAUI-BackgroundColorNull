8cecc5b592c3f732724c1dd377230a3382b6d0e2
Jonathan <jgarciag1404@alumno.ipn.mx>
2024-06-07 11:12:20 -0600

    Simple reproduction

---
+++ MauiApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiTest
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            this.BackgroundColor = null;
+        }
+    }
+
+}
